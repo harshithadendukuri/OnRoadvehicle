bd7363991ee9c89e157174ea48795d9a1e11cf7c
Lakshmi Harshitha <harshithadendukuri@gmai.com>
2023-03-14 21:39:15 +0530

    on road

---
+++ OnRoadVehicle/src/main/java/com/g3/OnRoadVehicle/OnRoadVehicleApplication.java
@@ -0,0 +1,13 @@
+package com.g3.OnRoadVehicle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnRoadVehicleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnRoadVehicleApplication.class, args);
+	}
+
+}
